f81fd7a0b036ad76666786302a0e5237790946d7
adfar-136 <adfarrasheed136@gmail.com>
2024-09-03 20:46:05 +0530

    dom started

---
+++ DOM/first.js
@@ -0,0 +1,26 @@
+// document.write("Hello Motto")
+// let element = document.getElementById("main")
+// console.log(element)
+// function fun(){
+//     element.innerHTML="<mark>Hello welcome finally dom started</mark>"
+//     element.style.backgroundColor="red";
+// }
+// let ele = document.getElementsByClassName("para")
+// console.log(ele)
+// function fun(){
+//     for(let i =0;i<ele.length;i++){
+//         ele[i].innerHTML="asdfghjuytre"
+//         ele[i].style.color="blue"
+//         ele[i].style.border="4px solid red"
+//     }
+// }
+// let elele= document.getElementsByTagName("h1")
+// console.log(elele)
+// let element = document.querySelectorAll(".para")
+// for(let i =0;i<element.length;i++){
+//     element[i].innerHTML="asdfghjuytre"
+// }
+let element = document.querySelector("div > p")
+element.setAttribute("title","heyyyyyy")
+element.removeAttribute("class")
+// element.style.backgroundColor="red"
--- objects.js
+++ objects.js
@@ -1,26 +1,27 @@
-let adfar = {
-  firstName : "ADfar",
-  lastName:"Rasheed",
-  age: 27,
-}
-console.log(adfar["firstName"])
-var arr = [12,"adfar",true,[1,2,3],{
-    name:"adfar",
-    age:27
-}]
-console.log(arr[4].name)
-var obj = {
-    name:"adfar",
-    age:27,
-    sayMyName:function(){
-        console.log(obj.name)
-    }
-}
-obj.sayMyName()
-// var data = [
-//     {},
-//     {},
-//     {}
-// ]
-console.log(arr)
-console.log(adfar.firstName)
+// let adfar = {
+//   firstName : "ADfar",
+//   lastName:"Rasheed",
+//   age: 27,
+// }
+// console.log(adfar["firstName"])
+// var arr = [12,"adfar",true,[1,2,3],{
+//     name:"adfar",
+//     age:27
+// }]
+// console.log(arr[4].name)
+// var obj = {
+//     name:"adfar",
+//     age:27,
+//     sayMyName:function(){
+//         console.log(obj.name)
+//     }
+// }
+// obj.sayMyName()
+// // var data = [
+// //     {},
+// //     {},
+// //     {}
+// // ]
+// console.log(arr)
+// console.log(adfar.firstName)
+console.log(document)
