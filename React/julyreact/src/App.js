ba0ed2cca70635b5e2d2e0df8cf469dcea031497
adfar-136 <adfarrasheed136@gmail.com>
2024-10-01 22:57:20 +0530

    completed controlled components

--- React/julyreact/src/App.js
+++ React/julyreact/src/App.js
@@ -1,16 +1,46 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
-function App() {
-  const [condition,setCondition] = useState(true)
-  function handleTheme(){
-    setCondition(!condition)
+export default function App() {
+  const [data,setData] =useState({
+    name:'Name',
+    email:"email",
+    pass:"password"
+  })
+
+  const [condition,setCondition] =useState(false)
+  function handleSubmit(e){
+    e.preventDefault()
+    setCondition(true)
+    // setUserName("")
+    // setPass("")
   }
   return (
-    <div style={{backgroundColor:condition?"black":"white",color:condition?"white":"black"}}>
-      <h1>Hello world</h1>
-      <button onClick={handleTheme}>{condition?"LIght":"Dark"} Mode</button>
+    <div>
+      <h1>Hello, world!</h1>
+     
+      <form onSubmit={handleSubmit}>
+        <label>UserName : </label>
+        <input type="text" value={data.name} 
+        onChange={(e)=>setData((prev)=>({...prev,name:e.target.value}))}/> <br />
+        <label>Email : </label>
+        <input type="email" 
+        value={data.email} 
+        onChange={(e)=>setData((prev)=>({...prev,email:e.target.value}))}/> <br />
+        <label>Password : </label>
+        <input type="password" 
+        value={data.pass} 
+        onChange={(e)=>setData((prev)=>({...prev,pass:e.target.value}))}/> <br />
+        <button type="submit">Login</button>
+        {data.name}  p
+        {data.pass}  p
+        {data.email}  p
+       {condition && (
+         <h1>
+         my name is {data.name}  and my password is:{data.pass}
+       </h1>
+       )}
+      </form>
     </div>
   )
 }
-
-export default App
